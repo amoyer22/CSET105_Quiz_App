ec617ed236d1be49f51f6398404634ddb431542f
Ailish <snickrrdoodles@gmail.com>
2024-10-23 08:17:28 -0400

    Created array of objects with questions and answers

---
+++ script.js
@@ -0,0 +1,52 @@
+let quiz = [
+    {
+        "number": 1,
+        "question": "According to Google, what year was JavaScript invented?",
+        "answer": 1995
+    },
+    {
+        "number": 2,
+        "question": "What's 2+2?",
+        "answer": 4
+    },
+    {
+        "number": 3,
+        "question": "Which of the following fruits are typically red?",
+        "answer": "Apple"
+    },
+    {
+        "number": 4,
+        "question": "How long does it take for me to drive to school everyday?",
+        "answer": "30 minutes"
+    },
+    {
+        "number": 5,
+        "question": "How much wood could a woodchuck chuck...",
+        "answer": "...if a woodchuck could chuck wood?"
+    },
+    {
+        "number": 6,
+        "question": "What question number are you on now?",
+        "answer": 6
+    },
+    {
+        "number": 7,
+        "question": "What is my favorite number?",
+        "answer": 22
+    },
+    {
+        "number": 8,
+        "question": "What is my favorite word?",
+        "answer": "Exit"
+    },
+    {
+        "number": 9,
+        "question": "What is my favorite color?",
+        "answer": "Hot pink"
+    },
+    {
+        "number": 10,
+        "question": "How long did it take for me to think of questions for this?",
+        "answer": "20 minutes"
+    }
+]
