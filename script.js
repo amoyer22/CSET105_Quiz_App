c0ac5362c5b44e063b642c93577fa84259dca051
Ailish <snickrrdoodles@gmail.com>
2024-10-23 09:52:30 -0400

    Updated quiz array to hold answer options

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 let currentPage = 0
 let quiz = [
     {
-        "number": 1,
         "question": "According to Google, what year was JavaScript invented?",
-        "answer": 1995
+        "options": ["2023", "1995", "1985", "2003"],
+        "answer": "1995"
     },
     {
-        "number": 2,
         "question": "What's 2+2?",
-        "answer": 4
+        "options": ["I don't know math", "undefined", "3", "4"],
+        "answer": "4"
     },
     {
-        "number": 3,
         "question": "Which of the following fruits are typically red?",
+        "options": ["Apple", "Orange", "Banana", "Watermelon"],
         "answer": "Apple"
     },
     {
-        "number": 4,
         "question": "How long does it take for me to drive to school everyday?",
+        "options": ["12 years", "30 minutes", "15 minutes", "3 seconds"],
         "answer": "30 minutes"
     },
     {
-        "number": 5,
         "question": "How much wood could a woodchuck chuck...",
+        "options": ["...What?", "...if a woodchuck could chuck wood?", "...700 pounds of wood, actually.", "...Who?"],
         "answer": "...if a woodchuck could chuck wood?"
     },
     {
-        "number": 6,
         "question": "What question number are you on now?",
-        "answer": 6
+        "options": ["3", "5", "6", "5.591"],
+        "answer": "6"
     },
     {
-        "number": 7,
         "question": "What is my favorite number?",
-        "answer": 22
+        "options": ["22", "33", "7", "8"],
+        "answer": "22"
     },
     {
-        "number": 8,
         "question": "What is my favorite word?",
+        "options": ["Boolean", "Sassafras", "Interesting", "Exit"],
         "answer": "Exit"
     },
     {
-        "number": 9,
         "question": "What is my favorite color?",
+        "options": ["Hot pink", "A deep purple", "Literally just blue", "Bright neon green"],
         "answer": "Hot pink"
     },
     {
-        "number": 10,
         "question": "How long did it take for me to think of questions for this?",
+        "options": ["These are questions?", "Who knows", "20 minutes", "All day"],
         "answer": "20 minutes"
     }
 ]
